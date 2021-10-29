8086026d08c5ea287d138971d8b5c8b284adac52
Lev Tartykov <lev_tartykov@mail.ru>
2021-10-29 22:00:45 +0300

    Создан .gitignore, удалены ненужные папки

--- build-cg_course-Desktop-RelWithDebInfo/CMakeFiles/bmstu_cg_course_project_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/bmstu_cg_course_project_autogen"
-  "bmstu_cg_course_project_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bmstu_cg_course_project_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
