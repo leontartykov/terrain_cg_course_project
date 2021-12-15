919cbfbe7cf96b206cc162754453d13c02c52147
Lev Tartykov <lev_tartykov@mail.ru>
2021-12-15 23:07:05 +0300

    Добавлено выполнение измерения времени

--- cg_course/interface/mainwindow.cpp
+++ cg_course/interface/mainwindow.cpp
@@ -1,5 +1,7 @@
 #include <cstdlib> // для функций rand() и srand()
 #include <ctime>
+#include <chrono>
+#include <fstream>
 
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
@@ -163,4 +165,89 @@ void MainWindow::scale_landscape()
     landscape.transform_points_to_screen();
     landscape.remove_invisible_lines(zbuffer, scene, light.get_position());
     landscape.draw_landscape(zbuffer, scene, view);
+}
+
+void MainWindow::measure_draw_landscape_time()
+{
+    rotate_t rotate_angles;
+    rotate_angles.angle_x = ui->spinbox_rotate_x->value();
+    rotate_angles.angle_y = ui->spinbox_rotate_y->value();
+    rotate_angles.angle_z = ui->spinbox_rotate_z->value();
+    double scale = ui->spinbox_scale->value();
+    landscape.set_scale_landscape(scale);
+
+    int array_size = 9;
+    int size[array_size] = {10, 25, 50, 100, 150, 250, 350, 500, 750};
+
+    std::chrono::duration<double> time_span;
+    std::chrono::high_resolution_clock::time_point t1, t2;
+    int count_repeats = 10;
+    std::ofstream file_out;
+    double time = 0;
+    file_out.open("time_1.csv");
+    file_out << "Размер ландшафта | Время \n" ;
+    for (int i = 0; i < array_size; i++){
+        qDebug() << "Размер ландшафта: " << size[i];
+        time = 0;
+        landscape.clear();
+        zbuffer.clear();
+        landscape.change_size(size[i], size[i]);
+        landscape.set_scale_landscape(scale);
+        landscape.set_meta_config(ui->spinbox_octaves->value(), ui->spinbox_gain->value(), ui->spinbox_lacunarity->value(),
+                                                    ui->spinbox_seed->value(), ui->spinbox_frequency->value());
+        landscape.set_rotate_angles(ui->spinbox_rotate_x->value(), ui->spinbox_rotate_y->value(), ui->spinbox_rotate_z->value());
+        for (int j = 0; j < count_repeats; j++)
+        {
+            t1 = std::chrono::high_resolution_clock::now();
+            landscape.form_landscape();
+            landscape.find_all_landscape_normals();
+            landscape.find_average_normals_of_each_node();
+            landscape.rotate_landscape(rotate_angles);
+            landscape.scale_landscape();
+            landscape.transform_points_to_screen();
+            landscape.remove_invisible_lines(zbuffer, scene, light.get_position());
+            landscape.draw_landscape(zbuffer, scene, view);
+            t2 = std::chrono::high_resolution_clock::now();
+            time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+            time += time_span.count();
+        }
+        time /= count_repeats;
+        file_out << size[i] << "   " << time << "\n";
+    }
+    qDebug() << "Замер времени закончено.";
+    file_out.close();
+}
+
+void MainWindow::measure_form_landscape_time()
+{
+    int size = 100;
+
+    std::chrono::duration<double> time_span;
+    std::chrono::high_resolution_clock::time_point t1, t2;
+    int count_octaves = 8;
+    int count_repeats = 10;
+    std::ofstream file_out;
+    double time = 0;
+    file_out.open("time_2.csv");
+    file_out << "Количество октав | Время\n" ;
+    for (int i = 1; i <= count_octaves; i++){
+        qDebug() << "Количество октав: " << i;
+        time = 0;
+        landscape.clear();
+        landscape.change_size(100, 100);
+        landscape.set_meta_config(i, ui->spinbox_gain->value(), ui->spinbox_lacunarity->value(),
+                                                    ui->spinbox_seed->value(), ui->spinbox_frequency->value());
+        for (int j = 0; j <= count_repeats; j++)
+        {
+            t1 = std::chrono::high_resolution_clock::now();
+            landscape.form_landscape();
+            t2 = std::chrono::high_resolution_clock::now();
+            time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+            time += time_span.count();
+        }
+        time /= count_repeats;
+        file_out << i << "   " << time << "\n";
+    }
+    qDebug() << "Замер времени закончено.";
+    file_out.close();
 }
